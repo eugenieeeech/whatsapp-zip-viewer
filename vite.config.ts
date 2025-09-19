99dea495b380780f78c0f8dae1e823258bcc92b6
Eugenie Cheng <56400884+eugenieeeech@users.noreply.github.com>
2025-09-20 01:37:05 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/whatsapp-zip-viewer/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
